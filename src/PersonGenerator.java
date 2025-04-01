d190865bc8e47791f9b50f7f071aae89a002691e
frickam <frickam@mail.uc.edu>
2025-04-01 11:12:30 -0400

    Initial commit

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -8,56 +8,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PersonGenerator {
-    public static void main(String[] args)
-    {
-        ArrayList<String> folks = new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         boolean done = false;
 
         File workingDirectory = new File(System.getProperty("user.dir"));
         Path file = Paths.get(workingDirectory.getPath() + "\\src\\personData.txt");
 
-        String personRec = "";
-        String ID = "";
-        String firstName = "";
-        String lastName = "";
-        String title = "";
-        int YOB = 0;
-
         do {
-            ID = SafeInput.getNonZeroLenString(in, "Enter the ID [6 digits]");
-            firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
-            lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
-            title = SafeInput.getNonZeroLenString(in, "Enter your title");
-            YOB = SafeInput.getRangedInt(in, "Enter your year of birth", 1000, 9999);
+            String ID = SafeInput.getNonZeroLenString(in, "Enter the ID [6 digits]");
+            String firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
+            String lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+            String title = SafeInput.getNonZeroLenString(in, "Enter your title");
+            int YOB = SafeInput.getRangedInt(in, "Enter your year of birth", 1000, 9999);
 
-            personRec = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
-            folks.add(personRec);
+            Person person = new Person(ID, firstName, lastName, title, YOB);
+            persons.add(person);
 
             done = SafeInput.getYNConfirm(in, "Are you done?");
+        } while (!done);
 
-        }while(!done);
-
-        for (String p: folks)
+        for (Person p : persons)
             System.out.println(p);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.toFile())))
-        {
 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.toFile()))) {
             // Finally can write the file LOL!
-
-            for(String rec : folks)
-            {
-                writer.write(rec, 0, rec.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
+            for (Person p : persons) {
+                writer.write(p.toCSV());
                 writer.newLine();  // adds the new line
-
             }
             writer.close(); // must close the file to seal it and flush buffer
             System.out.println("Data file written!");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/PersonReader.java
+++ src/PersonReader.java
@@ -3,49 +3,47 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class PersonReader
-{
+public class PersonReader {
     public static void main(String[] args) {
-
-
         JFileChooser chooser = new JFileChooser();
         Scanner inFile;
         String line;
+        ArrayList<Person> persons = new ArrayList<>();
         Path target = new File(System.getProperty("user.dir")).toPath();
         target = target.resolve("src");
         // Set the chooser to the project src directory
         chooser.setCurrentDirectory(target.toFile());
 
-        try
-        {
-            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-            {
+        try {
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 target = chooser.getSelectedFile().toPath();
                 inFile = new Scanner(target);
-                while (inFile.hasNextLine())
-                {
+                while (inFile.hasNextLine()) {
                     line = inFile.nextLine();
-                    System.out.println(line);
+                    String[] data = line.split(", ");
+                    if (data.length == 5) {
+                        Person person = new Person(data[0], data[1], data[2], data[3], Integer.parseInt(data[4]));
+                        persons.add(person);
+                    }
                 }
-
                 inFile.close();
 
-            }
-            else       // User did not pick a file, closed the chooser
-            {
+                // Print all Person objects to verify
+                for (Person person : persons) {
+                    System.out.println(person);
+                }
+            } else {
+                // User did not pick a file, closed the chooser
                 System.out.println("Sorry, you must select a file! Terminating!");
                 System.exit(0);
             }
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.out.println("File Not Found Error");
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
